b39fb52f21ee87873bf63b941208d7746d662ff7
Patrick W Larsen <patrickwlarsen@gmail.com>
2020-04-08 16:02:16 +0200

    playing around with request buffer and response headers

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const http = require('http');
 
+const todos = [
+  { id: 1, text: 'Todo One' },
+  { id: 2, text: 'Todo Two' },
+  { id: 3, text: 'Todo Three' }
+];
+
 const server = http.createServer((req, res) => {
-  const { headers, url, method } = req;
-  console.log(headers, url, method);
-  res.end();
+  res.writeHead(404, {
+    'Content-Type': 'application/json',
+    'X-Powered-By': 'Node.js'
+  });
+
+  let body = [];
+  req
+    .on('data', (chunk) => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+      console.log(body);
+    });
+
+  res.end(
+    JSON.stringify({
+      success: false,
+      error: 'Not Found',
+      data: null
+    })
+  );
 });
 
 const PORT = 5000;
