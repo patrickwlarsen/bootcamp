a7809b6829ef3dfa0b639b4c6d275818b2741886
Patrick W Larsen <patrickwlarsen@gmail.com>
2020-04-09 19:12:14 +0200

    - Added colors package for nice console logs - Created schema for bootcamp documents

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 /**
  * @description Get all bootcamps
  * @route       GET /api/v1/bootcamps
@@ -23,8 +25,17 @@ exports.getBootcamp = (req, res, next) => {
  * @route       POST /api/v1/bootcamps/:id
  * @access      Private
  */
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 /**
