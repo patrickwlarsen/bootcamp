6a92eb3db46ce80ec6c53acb460119aa3fa367e2
Patrick W Larsen <patrickwlarsen@gmail.com>
2020-04-09 18:18:50 +0200

    - Added morgan for http logging - Added mongoose for mongodb

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const logger = require('./middleware/logger');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
-// Mount middleware
-app.use(logger);
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server & exit process
+  server.close(() => process.exit(1));
+});
