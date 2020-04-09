c9e9b9928a8c1343c228a53480b0fdb2c5a04f5e
Patrick W Larsen <patrickwlarsen@gmail.com>
2020-04-09 16:50:28 +0200

    Added middleware logger

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const logger = require('./middleware/logger');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -8,6 +9,9 @@ const bootcamps = require('./routes/bootcamps');
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
+
+// Mount middleware
+app.use(logger);
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
