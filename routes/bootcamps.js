5e35d5f8318cfe062cdc75ce1b74409b42b61491
Patrick W Larsen <patrickwlarsen@gmail.com>
2020-04-18 14:04:20 +0200

    - Addded authorize to auth middleware - Specific routes in bootcamps and courses now require certain roles

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -11,7 +11,7 @@ const {
 
 const Bootcamp = require('../models/Bootcamp');
 const advancedResults = require('../middleware/advancedResults');
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
 // Include other resource routers
 const courseRouter = require('./courses');
@@ -24,17 +24,19 @@ router.use('/:bootcampId/courses', courseRouter);
 router.route('/radius/:zipcode/:distance/:unit').get(getBootcampsInRadius);
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route('/:id/photo').put(protect, bootcampPhotoUpload);
+router
+  .route('/:id/photo')
+  .put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 router
   .route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
