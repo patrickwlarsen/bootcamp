d63146c0e0e46567e6e3dcc9a3d741d48b35a88b
Patrick W Larsen <patrickwlarsen@gmail.com>
2020-04-18 13:47:45 +0200

    - Added auth middleware to protect routes - Added auth/me route to retrieve user data

--- controllers/auth.js
+++ controllers/auth.js
@@ -51,6 +51,16 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+/**
+ * @description Get current logged in user
+ * @route       POST /api/v1/auth/me
+ * @access      Private
+ */
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({ success: true, data: user });
+});
+
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
--- routes/courses.js
+++ routes/courses.js
@@ -9,6 +9,7 @@ const {
 
 const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
+const { protect } = require('../middleware/auth');
 
 const router = express.Router({ mergeParams: true });
 
@@ -18,7 +19,11 @@ router
     advancedResults(Course, { path: 'bootcamp', select: 'name description' }),
     getCourses
   )
-  .post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+  .post(protect, addCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(protect, updateCourse)
+  .delete(protect, deleteCourse);
 
 module.exports = router;
